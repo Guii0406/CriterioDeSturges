4a9087325c0e464788a3ed0429ee58cef95d15d8
Guilherme Belem <guilhermerblm2@gmail.com>
2020-10-02 02:22:23 -0300

    Primeiro commit, sem formulario extra

---
+++ CriterioDeSturges/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CriterioDeSturges
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Location = new System.Drawing.Point(202, 30);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numericUpDown1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(102, 173);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(343, 147);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Classes";
+            this.columnHeader1.Width = 88;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "f";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "F";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "fr(%)";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Fr(%)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(431, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numeros adicionados: 0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(477, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 373);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CriterioDeSturges/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CriterioDeSturges
+{
+    public partial class Form1 : Form
+    {
+        List<double> ListaNumeros = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+            //double[] range = new double[20] { 75.0, 60.1, 74.6, 68.1, 64.3, 67.2, 75.3, 79.3, 66.4, 86.6, 80.0, 85.0, 72.5, 73.2, 68.9, 71.0, 81.3, 64.2, 73.0, 81.2 };
+            //ListaNumeros.AddRange(range);
+            //double[] range = new double[30] { 52, 27, 46, 15, 22, 20, 68, 73, 19, 30, 33, 58, 24, 35, 32, 27, 42, 30, 45, 40, 70, 21, 27, 50, 51, 31, 17, 20, 60, 63 };
+            //ListaNumeros.AddRange(range);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Adicionar();
+        }
+        private void Adicionar()
+        {
+            ListaNumeros.Add((double)numericUpDown1.Value);
+            numericUpDown1.Value = 0;
+            numericUpDown1.Focus();
+
+            label1.Text = $"Numeros adicionados: {ListaNumeros.Count}";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int quantidadeNumeros = ListaNumeros.Count;
+            double maiorNumero = ListaNumeros.Max();
+            double menorNumero = ListaNumeros.Min();
+            double k = 1 + 3.3 * Math.Log10(quantidadeNumeros);
+
+            int h = Convert.ToInt32(Math.Round((maiorNumero - menorNumero) / k));
+
+            //MessageBox.Show(h.ToString());
+
+            int novoMenornumero = Convert.ToInt32(menorNumero);
+
+            int somaFs = 0;
+            double somaPorcentagens = 0;
+
+            for (; novoMenornumero <= maiorNumero; novoMenornumero += h)
+            {
+                string primeiraCelula = $"{novoMenornumero} a {novoMenornumero + h}";
+                int f = 0;
+                foreach (double numero in ListaNumeros)
+                {
+                    if (numero >= novoMenornumero && numero < novoMenornumero + h)
+                    {
+                        f++;
+                    }
+                }
+                somaFs += f;
+                somaPorcentagens += CalcularPorcentagem(f);
+                string segundaCelula = f.ToString();
+                string terceiraCelula = somaFs.ToString();
+                string quartaCelula = CalcularPorcentagem(f).ToString();
+                string quintaCelula = somaPorcentagens.ToString();
+
+                string[] linha = new string[5] { primeiraCelula, segundaCelula, terceiraCelula, $"{quartaCelula}%", $"{quintaCelula}%"};
+
+                listView1.Items.Add(new ListViewItem(linha));
+            }
+            string[] total = new string[5] { "Total", somaFs.ToString(), "", $"{somaPorcentagens}%", "" };
+            listView1.Items.Add(new ListViewItem(total));
+
+        }
+
+        private double CalcularPorcentagem(int f)
+        {
+            double x = (100.0 * f) / ListaNumeros.Count;
+            x = Math.Round(x);
+            return x;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ListaNumeros.Clear();
+            listView1.Items.Clear();
+            label1.Text = "Numeros adicionados: 0";
+        }
+
+        private void numericUpDown1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 13)
+            {
+                Adicionar();
+            }
+        }
+    }
+}
